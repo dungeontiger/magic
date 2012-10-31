e19529b890931d717d41334fc8d63201106259f5
Stephen D. Gibson <stephen.d.gibson@gmail.com>
2012-10-31 18:22:49 -0400

    Add new classes

---
+++ Alamir.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Alamir is the AI that makes all the intelligent decisions.
+ * He is the 'AI'
+ */
+class Alamir
+{
+}
+?>
---
+++ AlamirTest.php
@@ -0,0 +1,6 @@
+<?php
+include_once "Alamir.php";
+class AlamirTest extends PHPUnit_Framework_TestCase
+{
+}
+?>
